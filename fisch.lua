295de76c85f4c535140bdfd56581d3173720c9a9
vfatzscripter <tunbet145@gmail.com>
2024-11-08 12:41:47 +0700

    Add files via upload

---
+++ fisch.lua
@@ -0,0 +1,209 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "fisch", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Tab = Window:MakeTab({
+	Name = "no key",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+Tab:AddButton({
+	Name = "speed hub",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
+  	end    
+})
+Tab:AddButton({
+	Name = "inf ware",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/Kitler69/InfiniteWare/refs/heads/main/Fisch",true))()
+  	end    
+})
+Tab:AddButton({
+	Name = "prestine hub",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/RSenix3/PrestineHub/refs/heads/main/PrestineHubV2"))()
+  	end    
+})
+Tab:AddButton({
+	Name = "",
+	Callback = function()
+      		loadstring(game:HttpGet('https://pastebin.com/raw/N4weEdmM'))()
+  	end    
+})
+Tab:AddButton({
+	Name = "open src",
+	Callback = function()
+      		loadstring(game:HttpGet("https://you.whimper.xyz/sources/rb/fisch.lua"))()
+  	end    
+})
+Tab:AddButton({
+	Name = "bonk hub",
+	Callback = function()
+      		loadstring(game:HttpGet("https://bonkhubloader.netlify.app",true))()
+  	end    
+})
+Tab:AddButton({
+	Name = "femboy hub",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/kylosilly/femboyware/refs/heads/main/Fisch.lua"))()
+  	end    
+})
+Tab:AddButton({
+	Name = "felmo",
+	Callback = function()
+      		loadstring(game:HttpGet("https://pastebin.com/raw/mWssqEPS"))()
+  	end    
+})
+Tab:AddButton({
+	Name = "basement hub",
+	Callback = function()
+	setclipboard("https://casca-key-system-ori.onrender.com") 
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/Cazzanos/The-basement/main/Basement%20hub", true))()
+  	end    
+})
+Tab:AddButton({
+	Name = "lol",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/Whoisgrace0/Main/main/LOL.lua",true))()
+  	end    
+})
+Tab:AddButton({
+	Name = "beta lyth",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/Lythicals/script/main/Fisch%20Beta.lua",true))()
+  	end    
+})
+Tab:AddButton({
+	Name = "jvree hub",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/yolobradda/fischjvrre/refs/heads/main/fishrelease"))()
+  	end    
+})
+
+local Tab = Window:MakeTab({
+	Name = "key system",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+Tab:AddButton({
+	Name = "ather",
+	Callback = function()
+      		setclipboard("https://ads.luarmor.net/get_key?for=AtherHub_Key-uOsEXGVZazzi") 
+      loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2529a5f9dfddd5523ca4e22f21cceffa.lua"))()
+  	end    
+})
+Tab:AddButton({
+	Name = "draco hub",
+	Callback = function()
+      		setclipboard(" https://discord.gg/dracohub") 
+      loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/cdc8ffd74b2c33f6c9f47b85f4b77c45.lua"))()
+  	end    
+})
+Tab:AddButton({
+	Name = "maxhub",
+	Callback = function()
+      		setclipboard("discord.gg/maxhub")
+      loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3f4c258405c294983a9b0e113dad3de7.lua"))()
+  	end    
+})
+Tab:AddButton({
+	Name = "xenonhub",
+	Callback = function()
+      		setclipboard(" https://www.xenonhub.xyz/") 
+      loadstring(game:HttpGet('https://www.xenonhub.xyz/loader.lua'))()
+  	end    
+})
+Tab:AddButton({
+	Name = "ashbornnhub",
+	Callback = function()
+      		setclipboard("https://discord.com/invite/nzXkxej9wa") 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ashborrn/AshborrnHub/main/AshbornnHubOfficial.lua",true))()
+  	end    
+})
+Tab:AddButton({
+	Name = "procheat",
+	Callback = function()
+      		loadstring(game:HttpGet('https://raw.githubusercontent.com/Hawk983/Phantom-/refs/heads/main/ProCheat%20EXE%20Fisch'))()
+setclipboard("https://discord.gg/HeGmmD4dJd")
+  	end    
+})
+Tab:AddButton({
+	Name = "lyth cheat",
+	Callback = function()
+      		setclipboard(" https://ads.luarmor.net/get_key?for=-hZLGTqokwelN")
+      loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/53be22e52466e056e4928630b76b4a8c.lua"))()
+  	end    
+})
+Tab:AddButton({
+	Name = "space hub",
+	Callback = function()
+      		setclipboard("https://discord.gg/DAFPXSvS") 
+      loadstring(game:HttpGet('https://raw.githubusercontent.com/ago106/ScriptsRoblox/refs/heads/main/NewKey'))()
+  	end    
+})
+Tab:AddButton({
+	Name = "spacerb for jjspolit and solara",
+	Callback = function()
+      		loadstring(game:HttpGet('https://raw.githubusercontent.com/ago106/ScriptsRoblox/refs/heads/main/NewKey'))()
+      setclipboard("https://discord.gg/spacerb") 
+  	end    
+})
+Tab:AddButton({
+	Name = "spacerb mobile",
+	Callback = function()
+      		loadstring(game:HttpGet('https://pastebin.com/raw/N4weEdmM'))()
+ setclipboard("https://discord.gg/spacerb") 
+  	end    
+})
+Tab:AddButton({
+	Name = "twentyone hub",
+	Callback = function()
+      		setclipboard(" https://discord.com/invite/Xq3TNt9jxW") 
+      loadstring(game:HttpGet("https://twentyonehub.vercel.app"))()
+  	end    
+})
+Tab:AddButton({
+	Name = "no name",
+	Callback = function()
+	setclipboard("https://keyguardian.org/a/14") 
+      		loadstring(game:HttpGet("https://noname-hub.fun/loader.lua"))()
+  	end    
+})
+Tab:AddButton({
+	Name = "lhk",
+	Callback = function()
+	setclipboard("https://linkvertise.com/1045829/fisch-script-vsc9nm?o=sharing") 
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/LHking123456/2BkNcMLxp7Fbu/refs/heads/main/Fisch"))()
+  	end    
+})
+Tab:AddButton({
+	Name = "niburu",
+	Callback = function()
+      		setclipboard("https://discord.gg/QtVVN9oD") 
+  	end    
+})
+
+local Tab = Window:MakeTab({
+	Name = "misc.",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+Tab:AddButton({
+	Name = "anti kick",
+	Callback = function()
+      		loadstring(game:HttpGet("https://pastebin.com/raw/FPfaukXN"))()
+  	end    
+})
+Tab:AddButton({
+	Name = "afk",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/KazeOnTop/Rice-Anti-Afk/main/Wind", true))()
+  	end    
+})
+Tab:AddButton({
+	Name = "inf yield aka admin",
+	Callback = function()
+      		loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+  	end    
+})
+
+
