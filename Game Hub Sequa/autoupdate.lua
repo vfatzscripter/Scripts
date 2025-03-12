d1666b86dcbee4681bfafc8a599aec5900dc4d10
vfatzscripter <tunbet145@gmail.com>
2025-03-12 23:50:30 +0700

    WOWW NEW SCRIPT HAS BEEN RELEASED YAY

---
+++ Game Hub Sequa/autoupdate.lua
@@ -0,0 +1,85 @@
+print("THIS IS OPEN SORCE") 
+print("loaded! ") 
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/NICKISBAD/Nick-s-Modded-KAVO-Lib/main/Nick'sModdedKavoLib.lua"))()
+
+local Window = Library.CreateLib("Game Hub Saqua BETA RELEASE", "Sentinel")
+
+local Tab = Window:NewTab("Info")
+
+local Section = Tab:NewSection("This is open sorced and also, keyless!")
+local Section = Tab:NewSection("Created on mobile and pc!")
+local Section = Tab:NewSection("Ui Library: Kavo")
+
+local Tab = Window:NewTab("Games")
+local Section = Tab:NewSection("Hi")
+Section:NewButton("dead rails", "ButtonInfo", function()
+    local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/NICKISBAD/Nick-s-Modded-KAVO-Lib/main/Nick'sModdedKavoLib.lua"))()
+local Window = Library.CreateLib("dead rails", "DarkTheme")
+local Tab = Window:NewTab("No Key")
+local Section = Tab:NewSection("This zone have no key!")
+
+Section:NewButton("somtank v2.7", "ButtonInfo", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/tpvip777/DeathRailTestV1/refs/heads/main/DeathRailSomtankV2.7'))()
+end)
+Section:NewButton("lunor", "ButtonInfo", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Just3itx/Lunor-Loadstrings/refs/heads/main/Loader'))()
+end)
+Section:NewButton("speed hub x", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
+end)
+Section:NewButton("dead rails script by tora", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/gumanba/Scripts/refs/heads/main/DeadRails", true))()
+end)
+Section:NewButton("spider x", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SpiderScriptRB/Dead-Rails-SpiderXHub-Script/refs/heads/main/SpiderXHub%202.0.txt"))()
+end)
+Section:NewButton("item teleport", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/thiennrb7/Script/refs/heads/main/Bringall"))()
+end)
+Section:NewButton("Tbao", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/tbao143/thaibao/refs/heads/main/TbaoHubDeadRails"))()
+end)
+Section:NewButton("speed wave", "ButtonInfo", function()
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/speedwavevip/scriptspeed/refs/heads/main/Dead%20Ralis.lua")))()
+end)
+Section:NewButton("zenox", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/255ac567ced3dcb9e69aa7e44c423f19.lua"))()
+end)
+
+local Tab = Window:NewTab("Key")
+local Section = Tab:NewSection("This zone have key!")
+
+Section:NewButton("merdoon", "ButtonInfo", function()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/Merdooon/skibidi-sigma-spec-ter/refs/heads/main/specter')))()
+end)
+Section:NewButton("spineWare", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SpineWare/UniversalLoader/refs/heads/main/Load"))()
+    setclipboard("https://discord.gg/Pk7VQqZvZr") 
+end)
+Section:NewButton("sypher hub", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/nocturnal631/Dead-ra/refs/heads/main/Mmm"))()
+end)
+
+end)
+Section:NewButton("", "ButtonInfo", function()
+    
+end)
+
+-scripts
+local Tab = Window:NewTab("Scripts")
+Section:NewButton("", "ButtonInfo", function()
+    
+end)
+
+-hubs
+local Tab = Window:NewTab("Hubs")
+Section:NewButton("", "ButtonInfo", function()
+    
+end)
+
+-gui
+local Tab = Window:NewTab("Gui")
+Section:NewButton("", "ButtonInfo", function()
+    
+end)
