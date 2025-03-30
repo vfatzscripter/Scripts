bd50bfd688d678b7f4aed47c7b743c2f67e21a6f
vfatzscripter <tunbet145@gmail.com>
2025-03-30 21:01:39 +0700

    Update autoupdate.lua

--- Game Hub Sequa/autoupdate.lua
+++ Game Hub Sequa/autoupdate.lua
@@ -74,12 +74,127 @@ local Tab = Window:NewTab("Scripts")
 Section:NewButton("Fly Gui V3", "Requested", function()
     loadstring(game:HttpGet('https://pastebin.com/raw/YSL3xKYU'))()
 end)
-
-
-local Tab = Window:NewTab("Hubs")
+Section:NewButton("infinite yield", "ButtonInfo", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+end)
 Section:NewButton("", "ButtonInfo", function()
     
 end)
+Section:NewButton("", "ButtonInfo", function()
+    
+end)
+Section:NewButton("", "ButtonInfo", function()
+    
+end)
+Section:NewButton("", "ButtonInfo", function()
+    
+end)
+Section:NewButton("", "ButtonInfo", function()
+    
+end)
+Section:NewButton("", "ButtonInfo", function()
+    
+end)
+Section:NewButton("", "ButtonInfo", function()
+    
+end)
+Section:NewButton("", "ButtonInfo", function()
+    
+end)
+Section:NewButton("", "ButtonInfo", function()
+    
+end)
+Section:NewButton("", "ButtonInfo", function()
+    
+end)
+Section:NewButton("", "ButtonInfo", function()
+    
+end)
+Section:NewButton("", "ButtonInfo", function()
+    
+end)
+Section:NewButton("", "ButtonInfo", function()
+    
+end)
+Section:NewButton("", "ButtonInfo", function()
+    
+end)
+
+
+local Tab = Window:NewTab("Hubs")
+Section:NewButton("Game Hub V6", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/GamerScripter/Game-Hub/main/loader"))()
+end)
+Section:NewButton("X Hub", "ButtonInfo", function()
+    local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
+
+local Window = Library.CreateLib("X hub", "Ocean")local Tab = Window:NewTab("Universal Scripts")local Section = Tab:NewSection("Universal") Section:NewButton("Infinite Yield", "Admin", function() loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Infinite-Yield-17029"))()
+    print("Clicked")
+end) Section:NewButton("?", ".", function() loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-TONS-OF-EXECUTORS-UIS-Krnl-Solara-krampus-codex-synapse-x-15775"))()
+    print("Clicked")
+end) Section:NewButton("Game hub", "Script Hub", function() loadstring(game:HttpGet("https://github.com/GamerScripter/Game_Hub_v2/raw/main/loader"))()
+    print("Clicked")
+end) Section:NewButton("Universal Hub", "Hub", function() loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Universal-Hub-14818"))()
+    print("Clicked")
+end) Section:NewButton("Rewind script", "FE", function() loadstring(game:HttpGet("https://raw.githubusercontent.com/0Ben1/fe./main/L"))()
+
+
+    print("Clicked")
+end)Section:NewButton("Speed gui", "?", function() loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Speed-gui-15897"))()
+    print("Clicked")
+end) Section:NewButton("Loop Gui", "Kavo", function() loadstring(game:HttpGet("https://raw.githubusercontent.com/legendary2130/Loop-gui-Version-1.0.0/main/source"))()
+
+
+    print("Clicked")
+end) Section:NewButton("Giga Chad hub", "V5.5", function() loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Gigachad-hub-v55-7278"))()
+    print("Clicked")
+end) Section:NewButton("Malay Hhb", "Op", function() loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Malay-hub-V2-15570"))()
+    print("Clicked")
+end) Section:NewButton("UNC HUB", "?", function() loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-UNC-LARGEST-HUB-MORE-THAN-1000-SCRIPTS-12219"))()
+    print("Clicked")
+end) local Tab = Window:NewTab("Bedwars")local Section = Tab:NewSection("Bedwars") Section:NewButton("Ai sploit", "OP", function() loadstring(game:HttpGet("https://raw.githubusercontent.com/AlSploit/AlSploit/main/AlSploit/Bedwars/Loader.lua",true))()
+    print("Clicked")
+end) Section:NewButton("Voidware", "Decent", function() loadstring(game:HttpGet("https://rawscripts.net/raw/BedWars-Epilogue-voidware-vape-v4-15982"))()
+    print("Clicked")
+end) Section:NewButton("Aurora", "Laggy", function() loadstring(game:HttpGet("https://raw.githubusercontent.com/cocotv666/Aurora/main/Aurora_Loader"))()
+
+
+    print("Clicked")
+end) local Tab = Window:NewTab("Blox fruits")local Section = Tab:NewSection("Blox fruits") Section:NewButton("Matsune Hub", "Hub", function() loadstring(game:HttpGet("https://rawscripts.net/raw/Blox-Fruits-Matsune-hub-V2-NO-KEY-14920"))()
+    print("Clicked")
+end) Section:NewButton("Chest Farm", "Money", function() loadstring(game:HttpGet("https://rawscripts.net/raw/Blox-Fruits-Chest-Farm-FREE-16050"))()
+    print("Clicked")
+end) local Tab = Window:NewTab("Lucky Block battle")local Section = Tab:NewSection("Lucky Block Battle") Section:NewButton("Quasar Hub", "Op", function() loadstring(game:HttpGet("https://rawscripts.net/raw/LUCKY-BLOCKS-Battlegrounds-Quasar-Hub-10424"))()
+    print("Clicked")
+end) Section:NewButton("Mythic Hub", "Script", function() loadstring(game:HttpGet("https://rawscripts.net/raw/LUCKY-BLOCKS-Battlegrounds-Mythic-Hub-12542"))()
+    print("Clicked")
+end) local Tab = Window:NewTab("Kamehameha")local Section = Tab:NewSection("Kamehameha") Section:NewButton("Inf money", "?", function() loadstring(game:HttpGet("https://rawscripts.net/raw/Kamehameha-Simulator-Infinite-power-and-x2-potion-15433"))()
+    print("Clicked")
+end) local Tab = Window:NewTab("Delta")local Section = Tab:NewSection("Delta Keyboard") Section:NewButton("Delta", "Keyboard", function() loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Delta-keyboard-Crack-15565"))()
+    print("Clicked")
+end)local Tab = Window:NewTab("Player settings")local Section = Tab:NewSection("Player settings")Section:NewButton("Speed", "Makes You fast", function() game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 100
+
+
+    print("Clicked")
+end)  Section:NewButton("Jumppower", "JUMP High", function() game.Players.LocalPlayer.Character.Humanoid.JumpPower = 100
+
+
+    print("Clicked")
+end) Section:NewButton("Inf Jumps", "Jump FLY", function() loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Inf-Jump-script-11981"))()
+    print("Clicked")
+end) local Tab = Window:NewTab("Blade ball")local Section = Tab:NewSection("Blade ball") Section:NewButton("Auto parry 1", "1", function() loadstring(game:HttpGet("https://rawscripts.net/raw/UPD-Blade-Ball-script-16743"))()
+    print("Clicked")
+end) Section:NewButton("Auto parry 2", "2", function() loadstring(game:HttpGet("https://rawscripts.net/raw/Blade-Ball-FFJ-Hub-V2-15772"))()
+    print("Clicked")
+end) local Tab = Window:NewTab("Pls donate")local Section = Tab:NewSection("Pls donate") Section:NewButton("Pls donate", "Unfair Hub", function() loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-unfair-Hub-3936"))()
+    print("Clicked")
+end) local Tab = Window:NewTab("Build a Boat for treasure")local Section = Tab:NewSection("Build a boat") Section:NewButton("Fly to gold", "Free gold ", function() loadstring(game:HttpGet("https://rawscripts.net/raw/Build-A-Boat-For-Treasure-utoFrm-Gold-13684"))()
+    print("Clicked")
+end) local Section = Tab:NewSection("Ez hub") Section:NewButton("Ez hub", "OP", function() loadstring(game:HttpGet("https://rawscripts.net/raw/Ez-Hub_168"))()
+    print("Clicked")
+end)
+end)
+
 
 local Tab = Window:NewTab("Gui")
 Section:NewButton("", "ButtonInfo", function()
